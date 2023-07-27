f15fe7eed5a25f47e9049cad994b922c27fde55d
ToyeSobayo <104690696+ToyeSobayo@users.noreply.github.com>
2023-07-26 20:54:51 -0400

    App.js

---
+++ api/src/App.js
@@ -0,0 +1,17 @@
+ import Home from './pages/Home.jsx'
+ import ProductList from './pages/ProductList.jsx'
+ import Cart from './pages/Cart.jsx'
+ import Product from './pages/Product.jsx'
+ import Login from './pages/Login.jsx'
+ import Register from './pages/Register.jsx'
+
+const App = () => {
+  return <Home/>
+  return <ProductList/>;
+  return <Cart/>
+  return <Product/>
+  return <Login/>
+  return <Register/>
+};
+
+export default App;
