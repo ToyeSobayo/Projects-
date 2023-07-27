9079fca1b224c10f2c50fb8d64bae511808d9cd6
ToyeSobayo <104690696+ToyeSobayo@users.noreply.github.com>
2023-07-26 20:55:24 -0400

    Add files via upload

---
+++ api/src/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.pinimg.com/564x/15/9b/2f/159b2facad647fbac168533b1cb284a9.jpg",
+      title: "SUMMER SALE",
+      desc: "30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://i.pinimg.com/564x/10/f9/13/10f91324320d5cf28824b53b3698d0f6.jpg",
+      title: "DON'T MISS OUT",
+      desc: "30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/564x/d2/61/bf/d261bf60839d27a9be65881a5f74fcba.jpg",
+      title: "GET DRIPPED OUT",
+      desc: "30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://i.pinimg.com/564x/1b/f4/34/1bf4342b029a47bf05e8c0423e6416d1.jpg",
+      title: "TOPS",
+    },
+    {
+      id: 2,
+      img: "https://i.pinimg.com/564x/2a/59/22/2a5922b6dcd00ae2c71ff783ca2f786c.jpg",
+      title: "BOTTOMS",
+    },
+    {
+      id: 3,
+      img: "https://i.pinimg.com/564x/97/a0/ef/97a0ef78d79eba93b294e5b919d01d6a.jpg",
+      title: "FOOTWEAR",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://i.pinimg.com/736x/84/c8/7e/84c87ec933e32a8a79037770fc921b56.jpg",
+    },
+    {
+      id:2,
+      img:"https://i.pinimg.com/564x/53/7a/24/537a24320c4fe2e055b4518f960428ee.jpg",
+    },
+    {
+      id:3,
+      img:"https://i.pinimg.com/736x/31/0c/23/310c230dcfed27c59c522fea8e23518c.jpg",
+    },
+    {
+      id:4,
+      img:"https://i.pinimg.com/736x/4f/e4/b9/4fe4b9e40f306fac0fb8b5863cc7cf48.jpg",
+    },
+    {
+      id:5,
+      img:"https://i.pinimg.com/564x/08/d1/63/08d163c4f824a4bf2ba527465fb69ead.jpg",
+    },
+    {
+      id:6,
+      img:"https://i.pinimg.com/736x/1c/52/87/1c52875f7b36a5fc18dd98c85283c414.jpg",
+    },
+    {
+      id:7,
+      img:"https://i.pinimg.com/564x/7f/73/b1/7f73b19a1d8e5cd60aaea3768f7d8617.jpg"
+    },
+    {
+      id:8,
+      img:"https://i.pinimg.com/564x/3c/0b/e4/3c0be4982f02abf823b90353cc30fd91.jpg"
+    }
+
+  ]
